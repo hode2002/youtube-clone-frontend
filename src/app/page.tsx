d89eab097c0aa075966c4c44fea939cd3b888cd1
D H V <hovande.dev.2002@gmail.com>
2025-02-16 23:22:59 +0700

    modify: update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,150 +1,31 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-import Image from 'next/image';
-import React from 'react';
-import useVideoStore from '@/lib/store';
-import Header from '@/components/Header';
-
-const initialVideos = [
-    {
-        id: 1,
-        title: 'Video 1',
-        channel: 'Channel 1',
-        thumbnail: 'https://via.placeholder.com/150',
-    },
-    {
-        id: 2,
-        title: 'Video 2',
-        channel: 'Channel 2',
-        thumbnail: 'https://via.placeholder.com/150',
-    },
-    {
-        id: 3,
-        title: 'Video 3',
-        channel: 'Channel 3',
-        thumbnail: 'https://via.placeholder.com/150',
-    },
-    {
-        id: 4,
-        title: 'Video 4',
-        channel: 'Channel 4',
-        thumbnail: 'https://via.placeholder.com/150',
-    },
-    // Thêm nhiều video hơn ở đây
-];
+import React, { useEffect } from 'react';
+import VideoCard from '@/components/VideoCard';
+import { useSidebar } from '@/components/ui/sidebar';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { useVideoStore } from '@/stores';
+import { getVideos } from '@/apiRequests';
 
 export default function Home() {
-    const router = useRouter();
     const videos = useVideoStore((state) => state.videos);
-    const setVideos = useVideoStore((state) => state.setVideos);
+    const { setOpen } = useSidebar();
 
-    React.useEffect(() => {
-        setVideos(initialVideos);
-    }, [setVideos]);
+    useEffect(() => {
+        (async () => {
+            const videos = await getVideos();
+            console.log(videos);
+        })();
+        setOpen(true);
+    }, []);
 
-    const handleLogout = async () => {
-        await fetch('http://localhost:3001/api/v1/auth/logout', {
-            method: 'POST',
-            headers: {
-                Authorization: 'Bearer ' + window.localStorage.getItem('access_token'),
-            },
-            credentials: 'include',
-        });
-        window.localStorage.removeItem('access_token');
-        router.push('/');
-    };
     return (
-        <>
-            <Header />
-            <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-                <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                    <Image
-                        className="dark:invert"
-                        src="/next.svg"
-                        alt="Next.js logo"
-                        width={180}
-                        height={38}
-                        priority
-                    />
-                    <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-                        <li className="mb-2">
-                            Get started by editing{' '}
-                            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-                                src/app/page.tsx
-                            </code>
-                            .
-                        </li>
-                        <li>Save and see your changes instantly.</li>
-                    </ol>
-
-                    <div className="flex gap-4 items-center flex-col sm:flex-row">
-                        <a
-                            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-                            href="http://localhost:3001/api/v1/auth/google"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <Image
-                                className="dark:invert"
-                                src="/vercel.svg"
-                                alt="Vercel logomark"
-                                width={20}
-                                height={20}
-                            />
-                            Login
-                        </a>
-                        <button
-                            onClick={handleLogout}
-                            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-                            rel="noopener noreferrer"
-                        >
-                            Logout
-                        </button>
-                    </div>
-                </main>
-                <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-                    <a
-                        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                        href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
-                        Learn
-                    </a>
-                    <a
-                        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                        href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            aria-hidden
-                            src="/window.svg"
-                            alt="Window icon"
-                            width={16}
-                            height={16}
-                        />
-                        Examples
-                    </a>
-                    <a
-                        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                        href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            aria-hidden
-                            src="/globe.svg"
-                            alt="Globe icon"
-                            width={16}
-                            height={16}
-                        />
-                        Go to nextjs.org →
-                    </a>
-                </footer>
+        <ScrollArea className="h-[calc(100vh-72px)]">
+            <div className="mx-4 grid grid-cols-4 gap-4">
+                {[...videos, ...videos, ...videos, ...videos, ...videos].map((video, index) => (
+                    <VideoCard key={video.id + index} video={video} hasAvatar={false} />
+                ))}
             </div>
-        </>
+        </ScrollArea>
     );
 }
