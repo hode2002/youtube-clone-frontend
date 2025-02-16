96d90059d439857758e2836bc159ed08b45f4b7f
D H V <hovande.dev.2002@gmail.com>
2025-02-16 23:10:59 +0700

    modify: login with popup

--- src/app/auth-success/page.tsx
+++ src/app/auth-success/page.tsx
@@ -1,18 +1,23 @@
 'use client';
 
 import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { useAuthStore } from '@/stores';
+import { fetchUserData } from '@/apiRequests';
+import { setItemToLocalStorage } from '@/lib/utils';
 export default () => {
-    const router = useRouter();
+    const { setAccessToken } = useAuthStore();
+
     useEffect(() => {
         const hash = window.location.hash;
         const params = new URLSearchParams(hash.replace('#', '?'));
         const token = params.get('token');
 
         if (token) {
-            localStorage.setItem('access_token', token);
+            window.close();
+            setAccessToken(token);
+            (async () => await fetchUserData())();
             window.history.replaceState(null, '', '/');
-            router.push('/');
+            setItemToLocalStorage('reload', 'true');
         }
     }, []);
 };
