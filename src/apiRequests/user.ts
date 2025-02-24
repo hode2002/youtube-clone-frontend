1a2818858a21700a3162b79a2e1f11235cb3ab72
D H V <hovande.dev.2002@gmail.com>
2025-02-24 18:46:14 +0700

    add: get user profile

--- src/apiRequests/user.ts
+++ src/apiRequests/user.ts
@@ -1,16 +1,7 @@
 import customAxios from '@/lib/axios';
-import { useAuthStore, useUserStore } from '@/stores';
-import { User } from '@/types';
+import { User } from '@/types/user';
 
-export const fetchUserData = async () => {
-    try {
-        const { setProfile } = useUserStore.getState();
-        const { accessToken } = useAuthStore.getState();
-        if (!accessToken) return;
-
-        const { data } = await customAxios.get<User>('/users/profile');
-        setProfile(data);
-    } catch (error) {
-        console.log(error);
-    }
+export const getUserProfile = async (): Promise<User> => {
+    const res = await customAxios.get('/users/profile');
+    return res.data.data;
 };
