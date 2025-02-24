9fe61287e8062ddc3ffd595d87a9597485fa96a9
D H V <hovande.dev.2002@gmail.com>
2025-02-24 18:49:06 +0700

    modify: change default toast background

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -29,7 +29,8 @@ const toastVariants = cva(
     {
         variants: {
             variant: {
-                default: 'border bg-background text-foreground',
+                default: 'border bg-foreground text-background',
+                // default: 'border bg-background text-foreground',
                 destructive:
                     'destructive group border-destructive bg-destructive text-destructive-foreground',
             },
